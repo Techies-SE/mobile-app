92ff328f9316e3f3c3268b358c95f77358ff22e0
sisshmue <sisshmue404@gmail.com>
2025-02-21 22:25:13 +0700

    modified ui

--- lib/auth/change_password.dart
+++ lib/auth/change_password.dart
@@ -19,263 +19,267 @@ class ChangePassword extends StatelessWidget {
         padding: EdgeInsets.symmetric(
           horizontal: 30,
         ),
-        child: Column(
-          children: [
-            Icon(
-              Icons.lock_outline_rounded,
-              size: 42,
-              color: Color(0xff343330),
-            ),
-            Text(
-              'Change Your Password',
-              style: GoogleFonts.inter(
-                fontSize: 24,
-                fontWeight: FontWeight.w500,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Icon(
+                Icons.lock_outline_rounded,
+                size: 42,
+                color: Color(0xff343330),
               ),
-            ),
-            Text(
-              textAlign: TextAlign.center,
-              'Your password has expired.'
-              ' Please create a new password to  continue.',
-              style: GoogleFonts.poppins(
-                fontSize: 13,
-              ),
-            ),
-            SizedBox(
-              height: 31,
-            ),
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Text('Current Password'),
-            ),
-            SizedBox(
-              height: 4,
-            ),
-            Container(
-              width: double.infinity,
-              height: 50,
-              padding: EdgeInsets.only(
-                left: 10,
-              ),
-              decoration: BoxDecoration(
-                color: Color(0xffD9F2EF),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: TextField(
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.only(top: 12),
-                  suffixIcon: Icon(
-                    CupertinoIcons.eye_slash,
-                    size: 22,
-                  ),
-                  suffixIconColor: textColorSecondary,
-                  hintText: 'Enter current password',
-                  hintStyle: GoogleFonts.poppins(
-                    fontSize: 13,
-                  ),
-                  border: InputBorder.none,
+              Text(
+                'Change Your Password',
+                style: GoogleFonts.inter(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w500,
                 ),
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Text('New Password'),
-            ),
-            SizedBox(
-              height: 4,
-            ),
-            Container(
-              width: double.infinity,
-              height: 50,
-              padding: EdgeInsets.only(
-                left: 10,
-              ),
-              decoration: BoxDecoration(
-                color: Color(0xffD9F2EF),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: TextField(
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.only(top: 12),
-                  suffixIcon: Icon(
-                    CupertinoIcons.eye_slash,
-                    size: 22,
-                    color: textColorSecondary,
-                  ),
-                  hintText: 'Enter new password',
-                  hintStyle: GoogleFonts.poppins(
-                    fontSize: 13,
-                  ),
-                  border: InputBorder.none,
+              Text(
+                textAlign: TextAlign.center,
+                'Your password has expired.'
+                ' Please create a new password to  continue.',
+                style: GoogleFonts.poppins(
+                  fontSize: 13,
                 ),
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Text('Confirm New Password'),
-            ),
-            SizedBox(
-              height: 4,
-            ),
-            Container(
-              width: double.infinity,
-              height: 50,
-              padding: EdgeInsets.only(
-                left: 10,
+              SizedBox(
+                height: 31,
               ),
-              decoration: BoxDecoration(
-                color: Color(0xffD9F2EF),
-                borderRadius: BorderRadius.circular(10),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text('Current Password'),
               ),
-              child: TextField(
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.only(top: 12),
-                  suffixIcon: Icon(
-                    CupertinoIcons.eye_slash,
-                    size: 22,
-                  ),
-                  suffixIconColor: textColorSecondary,
-                  hintText: 'Confirm new password',
-                  hintStyle: GoogleFonts.poppins(
-                    fontSize: 13,
-                  ),
-                  border: InputBorder.none,
+              SizedBox(
+                height: 4,
+              ),
+              Container(
+                width: double.infinity,
+                height: 50,
+                padding: EdgeInsets.only(
+                  left: 10,
                 ),
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Card(
-              color: searchAndMore,
-              child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 23.0, horizontal: 25),
-                child: SizedBox(
-                  width: double.infinity,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Password Requirements',
-                      ),
-                      SizedBox(
-                        height: 4,
-                      ),
-                      Row(
-                        children: [
-                          Icon(
-                            CupertinoIcons.checkmark,
-                            size: 12,
-                          ),
-                          SizedBox(
-                            width: 7,
-                          ),
-                          Text(
-                            'Minimum 8 characters',
-                          ),
-                        ],
-                      ),
-                      SizedBox(
-                        height: 5,
-                      ),
-                      Row(
-                        children: [
-                          Icon(
-                            CupertinoIcons.checkmark,
-                            size: 12,
-                          ),
-                          SizedBox(
-                            width: 7,
-                          ),
-                          Text(
-                            'At least one uppercase letter',
-                          ),
-                        ],
-                      ),
-                      SizedBox(
-                        height: 5,
-                      ),
-                      Row(
-                        children: [
-                          Icon(
-                            CupertinoIcons.checkmark,
-                            size: 12,
-                          ),
-                          SizedBox(
-                            width: 7,
-                          ),
-                          Text(
-                            'At least one number',
-                          ),
-                        ],
-                      ),
-                      SizedBox(
-                        height: 5,
-                      ),
-                      Row(
-                        children: [
-                          Icon(
-                            CupertinoIcons.checkmark,
-                            size: 12,
-                          ),
-                          SizedBox(
-                            width: 7,
-                          ),
-                          Text(
-                            'At least one special character',
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            Spacer(),
-            TextButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => VerificationDone(),
-                  ),
-                );
-              },
-              style: TextButton.styleFrom(
-                minimumSize: Size(339, 64),
-                backgroundColor: mainBgColor,
-                shape: RoundedRectangleBorder(
+                decoration: BoxDecoration(
+                  color: Color(0xffD9F2EF),
                   borderRadius: BorderRadius.circular(10),
                 ),
-              ),
-              child: Text(
-                'Change Password',
-                style: GoogleFonts.poppins(
-                  fontSize: 16,
-                  color: Colors.white,
+                child: TextField(
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.only(top: 8),
+                    suffixIcon: Icon(
+                      CupertinoIcons.eye_slash,
+                      size: 22,
+                    ),
+                    suffixIconColor: textColorSecondary,
+                    hintText: 'Enter current password',
+                    hintStyle: GoogleFonts.poppins(
+                      fontSize: 13,
+                    ),
+                    border: InputBorder.none,
+                  ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              'Need help? Contact Support',
-              style: GoogleFonts.poppins(
-                fontSize: 12,
+              SizedBox(
+                height: 20,
               ),
-            ),
-            SizedBox(
-              height: 10,
-            )
-          ],
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text('New Password'),
+              ),
+              SizedBox(
+                height: 4,
+              ),
+              Container(
+                width: double.infinity,
+                height: 50,
+                padding: EdgeInsets.only(
+                  left: 10,
+                ),
+                decoration: BoxDecoration(
+                  color: Color(0xffD9F2EF),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.only(top: 8),
+                    suffixIcon: Icon(
+                      CupertinoIcons.eye_slash,
+                      size: 22,
+                      color: textColorSecondary,
+                    ),
+                    hintText: 'Enter new password',
+                    hintStyle: GoogleFonts.poppins(
+                      fontSize: 13,
+                    ),
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text('Confirm New Password'),
+              ),
+              SizedBox(
+                height: 4,
+              ),
+              Container(
+                width: double.infinity,
+                height: 50,
+                padding: EdgeInsets.only(
+                  left: 10,
+                ),
+                decoration: BoxDecoration(
+                  color: Color(0xffD9F2EF),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.only(top: 8),
+                    suffixIcon: Icon(
+                      CupertinoIcons.eye_slash,
+                      size: 22,
+                    ),
+                    suffixIconColor: textColorSecondary,
+                    hintText: 'Confirm new password',
+                    hintStyle: GoogleFonts.poppins(
+                      fontSize: 13,
+                    ),
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Card(
+                color: searchAndMore,
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 23.0, horizontal: 25),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Password Requirements',
+                        ),
+                        SizedBox(
+                          height: 4,
+                        ),
+                        Row(
+                          children: [
+                            Icon(
+                              CupertinoIcons.checkmark,
+                              size: 12,
+                            ),
+                            SizedBox(
+                              width: 7,
+                            ),
+                            Text(
+                              'Minimum 8 characters',
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Row(
+                          children: [
+                            Icon(
+                              CupertinoIcons.checkmark,
+                              size: 12,
+                            ),
+                            SizedBox(
+                              width: 7,
+                            ),
+                            Text(
+                              'At least one uppercase letter',
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Row(
+                          children: [
+                            Icon(
+                              CupertinoIcons.checkmark,
+                              size: 12,
+                            ),
+                            SizedBox(
+                              width: 7,
+                            ),
+                            Text(
+                              'At least one number',
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Row(
+                          children: [
+                            Icon(
+                              CupertinoIcons.checkmark,
+                              size: 12,
+                            ),
+                            SizedBox(
+                              width: 7,
+                            ),
+                            Text(
+                              'At least one special character',
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => VerificationDone(),
+                    ),
+                  );
+                },
+                style: TextButton.styleFrom(
+                  minimumSize: Size(339, 64),
+                  backgroundColor: mainBgColor,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                child: Text(
+                  'Change Password',
+                  style: GoogleFonts.poppins(
+                    fontSize: 16,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                'Need help? Contact Support',
+                style: GoogleFonts.poppins(
+                  fontSize: 12,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              )
+            ],
+          ),
         ),
       ),
     );
